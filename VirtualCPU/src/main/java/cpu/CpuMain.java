95f63ea523cbedb9d8405a33b5a7acb6193421dd
stinaanita <stina-stud@live.dk>
2017-05-12 10:49:33 +0200

    Added intructions for future use. And went over the project again to better remember all the work i did yesterday. In the examn I want the instructions implemented though a text file, but first i need about ten more else if's to use them.

--- VirtualCPU/src/main/java/cpu/CpuMain.java
+++ VirtualCPU/src/main/java/cpu/CpuMain.java
@@ -16,31 +16,31 @@ public class CpuMain
         "01001010", "00010000", "00001100", "11000110", "00010010", "00001111", 
         "00110010", "00000111", "10001100", "01000010", "00100001", "00011000",
         "00010000", "00010111", "00010000", "00001100", "11000110", "00010011", "00010010", "00000010", "00100001", "00011000");
-//                                      // MOV 5 A  
-                                        // PUSH A 
-//                                      // ALWAYS
-//                                      // CALL FACT
-//                                      // POP A 
-//                                      // HALT
-
-//                                      // MOV +1 A
-//                                      // NZERO
-//                                      // JMP RECUR
-//                                      // MOV 1 A
-//                                      // MOV A 1+
-//                                      // RTN
-
-//                                      // PUSH A
-//                                      // DEC
-//                                      // PUSH A
-//                                      // ALWAYS
-//                                      // CALL FACT
-//                                      // POP B
-//                                      // POP A
-//                                      // MUL
-//                                      // MOV A +1
-//                                      // RTN +0
-                
+//                                       MOV 5 A  
+//                                       PUSH A 
+//                                       ALWAYS
+//                                       CALL FACT
+//                                       POP A 
+//                                       HALT
+//
+//                                       MOV +1 A
+//                                       NZERO
+//                                       JMP RECUR
+//                                       MOV 1 A
+//                                       MOV A 1+
+//                                       RTN
+//
+//                                       PUSH A
+//                                       DEC
+//                                       PUSH A
+//                                       ALWAYS
+//                                       CALL FACT
+//                                       POP B
+//                                       POP A
+//                                       MUL
+//                                       MOV A +1
+//                                       RTN +0
+//                
         Machine machine = new Machine();
         machine.load(program);
         machine.print(System.out);
--- VirtualCPU/src/main/java/cpu/Machine.java
+++ VirtualCPU/src/main/java/cpu/Machine.java
@@ -15,168 +15,202 @@ public class Machine {
     }
 
     public void tick() {
-        // Get the next instruction and make shure it is not unsigned
-        int instr = memory.get(cpu.getIp());
+        int instr = memory.get(cpu.getIp()); // Get the next instruction and make shure it is not unsigned
 
-        if (instr == 0b0000_0000) { // 0000 0000  NOP no operation
+        if (instr == 0b0000_0000) { 
+        //  NOP no operation    0000 0000
             cpu.incIp();
-        } else if (instr == 0b0000_0001) { // 0000 0001 ADD A B
+        } 
+        else if (instr == 0b0000_0001) { 
+        //  ADD A B      0000 0001 
             cpu.setA(cpu.getA() + cpu.getB());
             cpu.setIp(cpu.getIp() + 1);
-        } else if (instr == 0b0000_0010) 
-        { // MUL A B
+        } 
+        else if (instr == 0b0000_0010){  
+        //  MUL A B
             cpu.setA(cpu.getA() * cpu.getB());
             cpu.setIp(cpu.getIp() + 1);
-        } else if (instr == 0b0000_0011) 
-        { // DIV A B
+        } 
+        else if (instr == 0b0000_0011){ 
+        // DIV A B
             cpu.setA(cpu.getA() / cpu.getB());
             cpu.setIp(cpu.getIp() + 1);
-        } else if (instr == 0b0000_0100) 
-        { // ZERO |	F ← A = 0; IP++
+        } 
+        else if (instr == 0b0000_0100) { 
+        // ZERO |	F ← A = 0; IP++
             cpu.setFlag(false); // Not shure
             cpu.incIp();
-        } else if (instr == 0b0000_0101) { // NEG |	F ← A < 0; IP++
+        } 
+        else if (instr == 0b0000_0101) { 
+        // NEG |	F ← A < 0; IP++
             if (cpu.getA() < 0) {
                 cpu.setFlag(true);
             }
             cpu.incIp();
-        } else if (instr == 0b0000_0110) { // POS |	F ← A > 0; IP++
+        } 
+        else if (instr == 0b0000_0110) { 
+        // POS |	F ← A > 0; IP++
             if (cpu.getA() > 0) {
                 cpu.setFlag(true);
             }
             cpu.incIp();
-        } else if (instr == 0b0000_0111) { // NZERO |    F ← A ≠ 0; IP++
+        } 
+        else if (instr == 0b0000_0111) { 
+        // NZERO |    F ← A ≠ 0; IP++
             if (cpu.getA() != 0 ) {
                 cpu.setFlag(true);
-            }else{cpu.setFlag(false);}
+            }
+            else cpu.setFlag(false);
             cpu.incIp();
-        } else if (instr == 0b0000_1000) { // EQ |	F ← A = B; IP++
+        } 
+        else if (instr == 0b0000_1000) { 
+        // EQ |	F ← A = B; IP++
             if (cpu.getA() == cpu.getB()) {
                 cpu.setFlag(true);
             }
             cpu.incIp();
-        } else if (instr == 0b0000_1001) {// LT |	F ← A < B; IP++
+        } 
+        else if (instr == 0b0000_1001) {
+        // LT |	F ← A < B; IP++
             if (cpu.A < cpu.getB()) {
                 cpu.setFlag(true);
             }
             cpu.incIp();
-        } else if (instr == 0b0000_1010) { // GT |	F ← A > B; IP++
+        } 
+        else if (instr == 0b0000_1010) { 
+        // GT |	F ← A > B; IP++
             if (cpu.B < cpu.getA()) {
                 cpu.setFlag(true);
             }
             cpu.incIp();
-        }else if((instr & 0b1111_1000)== 0b0001_1000)
-        { // RTN +o
+        }
+        else if((instr & 0b1111_1000)== 0b0001_1000){ 
+        // RTN +o
             int o = (instr & 0b0000_0111);
-            //IP<-[sp++]
+            // IP <- [sp++]
             cpu.setIp(memory.get(cpu.getSp()));
             cpu.setSp(cpu.getSp()+1);
-            //SP +=o
-            cpu.setSp(cpu.getSp() + o);
-            //IP++
+            cpu.setSp(cpu.getSp() + o); //SP +=o
             cpu.incIp();
         }
-        else if (instr == 0b0000_1011) { //NEQ |	F ← A ≠ B; IP++     
+        else if (instr == 0b0000_1011) { 
+        //NEQ |	F ← A ≠ B; IP++     
         
-        } else if (instr == 0b0000_1100) { // ALWAYS |   F ← true; IP++
+        } 
+        else if (instr == 0b0000_1100) { 
+        // ALWAYS |   F ← true; IP++
             cpu.setFlag(true);
             cpu.incIp();
-        } else if (instr == 0b0000_1101) { // Undefined
+        } 
+        else if (instr == 0b0000_1101) { // Undefined
             System.out.println("Undefined");
             cpu.incIp();
-        } else if (instr == 0b0000_1110) { // Undefined
+        } 
+        else if (instr == 0b0000_1110) { // Undefined
             System.out.println("Undefined");
             cpu.incIp();
-        } else if (instr == 0b0000_1111) 
+        } 
+        else if (instr == 0b0000_1111) 
         { //HALT  |	Halts execution
-        } else if ((instr & 0b1111_1110) == 0b0001_0000) 
+        } 
+        else if ((instr & 0b1111_1110) == 0b0001_0000) 
         {  // PUSH r  |  [--SP] ← r; IP++
             int r = instr & 0b0000_0001;
             cpu.decSp();
             if (r == Cpu.A) {
                 memory.set(cpu.getSp(), cpu.getA());
-            } else {
+            } 
+            else {
                 memory.set(cpu.getSp(), cpu.getB());
             }
             cpu.incIp();
-        } else if ((instr & 0b1111_1110) == 0b0001_0010) 
-        { // POP r  |  r ← [SP++]; IP++
+        } 
+        else if ((instr & 0b1111_1110) == 0b0001_0010) { 
+        // POP r  |  r ← [SP++]; IP++
             int r = instr & 0b0000_0001;
-            
             if (r == Cpu.A) {
                 cpu.setA(memory.get(cpu.getSp()));
-            } else {
+            } 
+            else {
                 cpu.setB(memory.get(cpu.getSp()));
             }
             cpu.setSp(cpu.getSp() + 1);
             cpu.incIp();
-        } else if (instr == 0b0001_0100)
-        { // Mov A B |  B ← A; IP++
+        } 
+        else if (instr == 0b0001_0100) { 
+        // Mov A B |  B ← A; IP++
             cpu.setB(cpu.getA());
             cpu.incIp();
-        } else if (instr == 0b0001_0101) 
-        { // Mov B A  |  A ← B; IP++
+        } else if (instr == 0b0001_0101) { 
+        // Mov B A  |  A ← B; IP++
             cpu.setA(cpu.getB());
             cpu.incIp();
-        }else if(instr == 0b0001_0111)// DEC
-        {
+        }
+        else if(instr == 0b0001_0111){
+        // DEC
             cpu.setA(cpu.getA()-1);
             cpu.incIp();
             
-        }else if(instr == 0b0001_0110) // INC
-        {
+        }else if(instr == 0b0001_0110){
+        // INC
             cpu.setA(cpu.getA() +1);
             cpu.incIp();
         }
-        else if ((instr & 0b1111_0000) == 0b0010_0000) 
-        { // MOV r o  |  [SP + o] ← r; IP++  
+        else if ((instr & 0b1111_0000) == 0b0010_0000) { 
+        // MOV r o  |  [SP + o] ← r; IP++  
             int o = instr & 0b0000_0111;
             int r = (instr & 0b0000_1000) >> 3;
             if (r == cpu.A) {
                 memory.set(cpu.getSp() + o, cpu.getA());
-            } else {
+            } 
+            else {
                 memory.set(cpu.getSp() + o, cpu.getB());
             }
             cpu.incIp();
-        } else if ((instr & 0b1111_0000) == 0b0011_0000) 
-        { // MOV  o r | r ← [SP + o]; IP++  0011 ooor
+        } 
+        else if ((instr & 0b1111_0000) == 0b0011_0000) { 
+        // MOV  o r | r ← [SP + o]; IP++  0011 ooor
             int o = (instr & 0b0000_1110) >> 1;
             int r = instr & 0b0000_0001;
             if (r == cpu.A) {
                 cpu.setA(memory.get(cpu.getSp() + o));
-            } else {
+            } 
+            else {
                 cpu.setB(memory.get(cpu.getSp() + o));
             }
             cpu.incIp();
-        } else if ((instr & 0b1100_0000) == 0b0100_0000)
-        { // MOV v r |  r ← v; IP++ 01vv vvvr
+        } 
+        else if ((instr & 0b1100_0000) == 0b0100_0000) { 
+        // MOV v r |  r ← v; IP++ 01vv vvvr
             int v = (instr & 0b0011_1110) >> 1; // the same as deviding by 2
             int r = instr & 0b0000_0001;
             if (r == Cpu.A) {
                 cpu.setA(v);
-            } else {
+            } 
+            else {
                 cpu.setB(v);
             }
             cpu.incIp();
-        } else if ((instr & 0b1100_0000) == 0b1100_0000) 
-        {// CALL 11AAAAAA   11aa aaaa
+        } 
+        else if ((instr & 0b1100_0000) == 0b1100_0000) {
+        // CALL 11AAAAAA   11aa aaaa
             int a = (instr & 0b0011_1111);
             if (cpu.isFlag()) {
                 cpu.decSp();
                 memory.set(cpu.getSp(), cpu.getIp());
                 cpu.setIp(a);
-            } else {
-                cpu.incIp();
-            }
-        } else if ((instr & 0b1000_0000) == 0b1000_0000) 
-        { // JMP #a
+            } 
+            else cpu.incIp();
+        
+        } 
+        else if ((instr & 0b1000_0000) == 0b1000_0000) { 
+        // JMP #a
             int a = (instr & 0b0011_1111);
 
             if (cpu.isFlag()) {
                 cpu.setIp(a);
-            } else {
-                cpu.incIp();
-            }
+            } 
+            else cpu.incIp();         
         }
     }
 
