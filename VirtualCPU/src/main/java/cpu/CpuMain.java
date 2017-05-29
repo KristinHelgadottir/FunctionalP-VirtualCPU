32655a4cf1426905f53921cf24cc99ff67650aa1
stinaanita <stina-stud@live.dk>
2017-05-29 14:23:07 +0200

    Added the tail recursive implementation.

--- VirtualCPU/src/main/java/cpu/CpuMain.java
+++ VirtualCPU/src/main/java/cpu/CpuMain.java
@@ -12,8 +12,8 @@ public class CpuMain
 
         System.out.println("Welcome to the awesome CPU program");
 
-        Program program = new Program(
-        "01001010", "00010000", "00001100", "11000110", "00010010", "00001111", 
+        Program programt = new Program(
+         "01001010", "00010000", "00001100", "11000110", "00010010", "00001111",
         "00110010", "00000111", "10001100", "01000010", "00100001", "00011000",
         "00010000", "00010111", "00010000", "00001100", "11000110", "00010011", "00010010", "00000010", "00100001", "00011000");
 //                                       MOV 5 A  
@@ -40,7 +40,36 @@ public class CpuMain
 //                                       MUL
 //                                       MOV A +1
 //                                       RTN +0
-//                
+        Program program = new Program(
+                "01000011", "00010000", "01001010", "00010000", "00001100", "11001000", "00010010", "00001111",
+                "00110010", "00000111", "10001100", "00011001",
+                "00110101", "00000010", "00100010", "00110010", "00010111", "00100001", "00001100", "10001000");//                                       MOV 5 A
+//                     MOV 1 A
+//                     PUSH A
+//                     MOV 5 A
+//                     PUSH A
+//                     ALWAYS
+//                     CALL FACT
+//                     POP A
+//                     HALT
+//
+//                     MOV +1 A
+//                     NZERO
+//                     JMP RECUR        MUNA AD BREYTA THESSU LIKA  = ...?
+//                     RTN +1
+//                     MOV +2 B         MOV o r  0011 ooor
+//                     MUL
+//                     MOV A +2  0010 rooo || 0010 0010
+//                     MOV +1 A  0011 ooor	|| 0011 0010
+//                     DEC		   			   0001 0111
+//                     MOV A +1  0010 rooo || 0010 0001
+//                     ALWAYS				   0000 1100
+//                     JMP 8
+
+
+
+
+
         Machine machine = new Machine();
         machine.load(program);
         machine.print(System.out);
